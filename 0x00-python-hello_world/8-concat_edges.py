4a0f142968dfbbf804a045206f5e7edcfcf5eb60
NouhanDoumbouya123 <norassobadoumbouya123@gmail.com>
2024-03-12 07:02:00 +0800

    con edges

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print(str[39:67] + str[107:112] + str[0:7])
+print(f"{str[39:67]}{str[107:112]}", end = "")
+print(f"{str[0:7]}")
