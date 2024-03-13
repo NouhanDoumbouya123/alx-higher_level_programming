5dd5e92e97b3a34923256cc641580009ececc659
NouhanDoumbouya123 <norassobadoumbouya123@gmail.com>
2024-03-13 15:37:25 +0800

    concat

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-str = """Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"""
-
-print(str[39:67], str[107:112], str[0:7])
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+# YOUR CODE 
+print((lambda s: s[39:67] + s[107:112] + s[0:7])(s := "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"))
+#print(str)
