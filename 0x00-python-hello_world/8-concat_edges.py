a0c098d8d1c9461f34246e25c632615c436d1158
NouhanDoumbouya123 <norassobadoumbouya123@gmail.com>
2024-03-13 15:24:56 +0800

    concat edges

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(f"{str[39:67]}{str[107:112]}", end = "")
-print(f"{str[0:7]}", end = "")
-print()
+print(str[39:67], str[107:112], str[0:7])
