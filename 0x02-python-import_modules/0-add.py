630b5269026f1982176eb16038b3e1c9bf209589
NouhanDoumbouya123 <norassobadoumbouya123@gmail.com>
2024-03-14 17:33:38 +0800

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-from add_0 import add
+    from add_0 import add
 
-a = 1
-b = 2
-#sum_two = add(a, b)
-print("{} + {} = {}".format(a, b, add(a, b)))
+    a = 1
+    b = 2
+    # sum_two = add(a, b)
+    print("{} + {} = {}".format(a, b, add(a, b)))
