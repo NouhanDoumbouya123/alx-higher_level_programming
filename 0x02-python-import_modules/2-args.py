e9fd9e8835585bb1ee3b08e703674d30619d133a
NouhanDoumbouya123 <norassobadoumbouya123@gmail.com>
2024-03-14 17:40:18 +0800

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 if __main__ == "__main__":
-import sys
+    import sys
 
-arguments = sys.argv
-length = len(arguments) - 1
-print("{} arguments.".format(length))
-index = 1
-for arg in arguments[1:]:
+    arguments = sys.argv
+    length = len(arguments) - 1
+    print("{} arguments.".format(length))
+    index = 1
+    for arg in arguments[1:]:
     print("{}: {}".format(index, arg))
     index += 1
