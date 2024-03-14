c210c6f858b03513e5b53845672c475f70dc6cea
NouhanDoumbouya123 <norassobadoumbouya123@gmail.com>
2024-03-14 16:02:31 +0800

    import add function

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+from add_0 import add
+
+a = 1
+b = 2
+sum_two = add(a, b)
+print("{} + {} = {}".format(a, b, sum_two))
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,1 @@
+This contains imported codes that are using imported modules
