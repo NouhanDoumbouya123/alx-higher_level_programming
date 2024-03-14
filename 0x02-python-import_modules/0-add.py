b92d0ce060b0644eb45cfcbe147f6e46d17f893b
NouhanDoumbouya123 <norassobadoumbouya123@gmail.com>
2024-03-14 17:27:42 +0800

    changement

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__"
+if __name__ == "__main__":
 from add_0 import add
 
 a = 1
 b = 2
-sum_two = add(a, b)
-print("{} + {} = {}".format(a, b, sum_two))
+#sum_two = add(a, b)
+print("{} + {} = {}".format(a, b, add(a, b)))
