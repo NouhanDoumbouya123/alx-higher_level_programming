c1de1fb978b3c01406540afd866d12e4e870c6a0
NouhanDoumbouya123 <norassobadoumbouya123@gmail.com>
2024-03-18 21:53:58 +0800

    finding the max value in a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    max_value = 0
+    max_value = my_list[0]
     if my_list:
         for i in my_list:
             if i > max_value:
