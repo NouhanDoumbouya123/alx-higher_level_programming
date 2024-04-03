64cedd108f116da26f23dcffe2f13b2dbebe658e
NouhanDoumbouya123 <norassobadoumbouya123@gmail.com>
2024-04-03 19:09:34 +0800

    instantiation with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defining a class Square based on the previous"""
+
+
+class Square:
+    """A square with instantiation and attribute"""
+
+    def __init__(self, size):
+        """Initialize a new Square instance."""
+        self.__size = size
