218f77f06c9a5619ee140f49fe27f8275ad7f235
NouhanDoumbouya123 <norassobadoumbouya123@gmail.com>
2024-04-12 04:22:39 +0800

    0-add

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,47 +1,19 @@
 #!/usr/bin/python3
-"""
-This module contains a function that adds two integers.
-"""
-
 
 def add_integer(a, b=98):
-    """
-    Adds two integers.
+    """This function adds two numbers whether they are integer of floats
 
     Args:
-        a (int or float): The first number.
-        b (int or float): The second number. Defaults to 98.
-
+        a: the first number
+        b: the second number
     Returns:
-        int: The sum of a and b.
-
+        returns an integer, the addition of a and b
     Raises:
-        TypeError: If a or b are not integers or floats.
-
-    Examples:
-        >>> add_integer(1, 2)
-        3
-        >>> add_integer(100, -2)
-        98
-        >>> add_integer(2)
-        100
-        >>> add_integer(100.3, -2)
-        98
-        >>> add_integer(4, "School")
-        Traceback (most recent call last):
-            ...
-        TypeError: b must be an integer
-        >>> add_integer(None)
-        Traceback (most recent call last):
-            ...
-        TypeError: a must be an integer
+        Typerror: raises if a or b are not integer or float
     """
-    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
-        raise TypeError("a must be an integer or b must be an integer")
-
-    return int(a) + int(b)
-
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    a, b = int(a), int(b)
+    return a + b
