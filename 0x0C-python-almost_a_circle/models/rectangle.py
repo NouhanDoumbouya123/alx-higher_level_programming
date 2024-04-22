2e51a6bca8bee6d4deb537e90e89451fe4cf411c
NouhanDoumbouya123 <norassobadoumbouya123@gmail.com>
2024-04-23 01:16:12 +0800

    As the checker is not checking some outputs, I have changed the way of checking the type of widht and height to see what it will give

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -41,7 +41,7 @@ class Rectangle(Base):
     def set_width(self, width):
         """To set the width"""
 
-        if not isinstance(width, int):
+        if type(widht) != int:
             raise TypeError("width must be an integer")
         if width <= 0:
             raise ValueError("width must be > 0")
@@ -50,7 +50,7 @@ class Rectangle(Base):
     def set_height(self, height):
         """To set the height"""
 
-        if not isinstance(height, int):
+        if type(height) != int:
             raise TypeError("height must be an integer")
         if height <= 0:
             raise ValueError("height must be > 0")
