24927bfc812addd9f063310c2b31a571ca3cf03e
NouhanDoumbouya123 <norassobadoumbouya123@gmail.com>
2024-05-22 00:31:52 +0800

    updated

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,9 +1,4 @@
--- Script to print the full description of the table first_table from the database hbtn_0c_0
-
--- Redirect the output of the SHOW CREATE TABLE statement to a temporary table
-CREATE TEMPORARY TABLE IF NOT EXISTS tmp_description AS
-    SHOW CREATE TABLE hbtn_0c_0.first_table;
-
--- Retrieve the full description of the table from the temporary table
-SELECT CONCAT_WS('\n', Create_Table, Create_View) AS 'Table Description'
-FROM tmp_description;
+-- Print the full description of a table
+SELECT *
+FROM information_schema.columns
+WHERE table_schema='hbtn_0c_0' AND table_name='first_table';
